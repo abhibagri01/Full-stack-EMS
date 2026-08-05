1adfa28657da41e246f40732b489a030179416d3
abhibagri01 <abhinavbagri174@gmail.com>
2026-08-05 20:40:22 +0530

    Payslip page UI completed

--- client/src/pages/Payslip.jsx
+++ client/src/pages/Payslip.jsx
@@ -1,7 +1,50 @@
+import React, { useState, useEffect, useCallback, use } from 'react';
+import { dummyEmployeeData, dummyPayslipData } from '../assets/assets';
+import Loading from '../components/Loading';
+import PayslipList from '../components/payslip/PayslipList';
+import GeneratePayslipForm from '../components/payslip/GeneratePayslipForm';
+
 
 const Payslip = () => {
+
+  const [payslips, setPayslips] = useState([]);
+  const [employee, setEmployee] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const isAdmin = true; // Replace with actual admin check logic
+  
+
+  const fetchPayslips = useCallback(async () => {
+    // Fetch payslips from API or use dummy data
+    setPayslips(dummyPayslipData);
+    setTimeout(() => {
+              setLoading(false);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    fetchPayslips();
+  }, [fetchPayslips]);
+
+  useEffect(() => {
+    if (isAdmin) {
+      // Fetch employee data for admin view or use dummy data
+      setEmployee(dummyEmployeeData);
+    }
+  }, [isAdmin]);
+
+  if (loading) return <Loading />;
+
   return (
-    <div>Payslip</div>
+    <div className='animate-fade-in'>
+      <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-8'>
+        <div>
+          <h1 className='page-title'>Payslips</h1>
+          <p className='page-subtitle'>{isAdmin ? "Generate and manage employee payslips" : "Your payslips history"}</p>
+        </div>
+        {isAdmin && <GeneratePayslipForm employee={employee} onSuccess={fetchPayslips} />}
+      </div>
+      <PayslipList payslips={payslips} isAdmin={isAdmin} />
+    </div>
   )
 }
 
