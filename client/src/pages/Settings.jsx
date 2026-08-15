e42bde3a7f34d2640b10d5ada34901b85b45e218
abhibagri01 <abhinavbagri174@gmail.com>
2026-08-15 16:23:51 +0530

    fronted completed

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,9 +1,55 @@
+import { useEffect, useState } from 'react'
+import { dummyProfileData } from '../assets/assets'
+import Loading from '../components/Loading'
+import { Lock } from 'lucide-react';
+import ProfileForm from '../components/ProfileForm';
+import ChangePasswordModal from '../components/ChangePasswordModal';
 
 
-const Setings = () => {
+const Settings = () => {
+  const [profile, setProfile] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [showPasswordModal, setShowPasswordModal] = useState(false)
+
+  const fetchProfile = async () => {
+    // Fetch profile data from API or use dummy data
+    setProfile(dummyProfileData)
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }
+
+  useEffect(() => {
+    fetchProfile()
+  }, [])
+
+  if (loading) return <Loading />
+
   return (
-    <div>Setings</div>
+    <div className='animate-fade-in'>
+      <div className='page-header'>
+        <h1 className='page-title'>Settings</h1>
+        <p className='page-subtitle'>Manage your account settings and preferences</p>
+      </div>
+
+      {profile && <ProfileForm initialData={profile} onSuccess={fetchProfile} />}
+
+      {/* Password Section  */}
+      <div className='card max-w-md p-6 flex items-center justify-between'>
+        <div className='flex items-center gap-3'>
+          <div>
+            <Lock className='w-5 h-5 text-slate-600' />
+          </div>
+          <div>
+            <p className='font-medium text-slate-500'>Password</p>
+            <p className='text-sm text-slate-500'>Update your password</p>
+          </div>
+        </div>
+        <button onClick={() => setShowPasswordModal(true)} className='btn-secondary text-sm'>Change Password</button>
+      </div>
+      <ChangePasswordModal open={showPasswordModal} onClose={() => setShowPasswordModal(false)} />
+    </div>
   )
 }
 
-export default Setings
+export default Settings
